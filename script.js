68a35aeccb3dc769259e6ca2217d43597d496a56
thexhainuser <thexhainuser@gmail.com>
2026-04-03 13:53:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-element.scrollIntoView({ behavior: "smooth" });
+const button = document.getElementById("scrollBtn");
+button.addEventListener("click", function() {
+  const section = document.querySelector(".black-section");
+  section.scrollIntoView({ behavior: "smooth" });
+});
