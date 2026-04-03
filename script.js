92c9a12cfa6205056a01251226e2ea283a2c963b
thexhainuser <thexhainuser@gmail.com>
2026-04-03 17:29:32 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("scrollBtn");
-button.addEventListener("click", function () {
-  const section = document.querySelector(".black-section");
-  section.scrollIntoView({ behavior: "smooth" });
-});
